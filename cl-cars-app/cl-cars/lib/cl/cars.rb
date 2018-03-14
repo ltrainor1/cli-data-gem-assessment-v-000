99b61bce846af678072c9dc024c0226b3d3d872e
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-14 04:35:22 +0000

    automatically backed up by learn

--- cl-cars-app/cl-cars/lib/cl/cars.rb
+++ cl-cars-app/cl-cars/lib/cl/cars.rb
@@ -20,7 +20,7 @@ end
 
   def self.cl_search(input)
     doc = Nokogiri::HTML(open('https://www.govtrack.us/congress/votes'))
-    list = doc.css('div#result_item')
+    list = doc.css('div.results')
     binding.pry
     list
 end
